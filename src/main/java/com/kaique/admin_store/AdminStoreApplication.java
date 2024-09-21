215a234f9d8166bf129210386750162a3d6126df
kaiquesimao <kaique.gabriel.me@gmail.com>
2024-09-21 19:21:46 -0300

    initial commit

---
+++ src/main/java/com/kaique/admin_store/AdminStoreApplication.java
@@ -0,0 +1,13 @@
+package com.kaique.admin_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaique/admin_store/AdminStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaique.admin_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
